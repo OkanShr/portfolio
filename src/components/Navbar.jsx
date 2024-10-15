2979cb6083f168864635a34ef2c06f953ede0dcc
OkanShr <okan.shr@hotmail.com>
2024-10-15 02:26:53 +0200

    Portfolio v1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { CONTACT } from "../constants";
+import Logo from "../assets/OkanLogobigger.png";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <h1 className="text-2xl">Xr</h1>
+        <img src={Logo} alt="OkanShr" width={40} />
+        <h1 className="text-2xl">Shr</h1>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href={CONTACT.gitHub} target="_blank" rel="noopener noreferrer">
